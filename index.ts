2c4fd7955e30297f2e56b34688cd4ca5366067a3
노 기 훈 <kiki01111@naver.com>
2025-09-02 01:00:47 +0900

    chore(setting): FSD Architecture 적용을 위한 최소한의 레이어 구분 - 팀원과 합의에 따라 app, shared, entities에서 점진적으로 확장 - fsd에 따른 폴더 alias 지정 (확장성을 고려하여 모든 레이어에 대한 별칭 추가) - web 환경의 테스트를 위한 의존성 추가

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { registerRootComponent } from 'expo';
-
-import App from './App';
+import { App } from './src/app';
 
 registerRootComponent(App);
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '@/global.css';
+
+export function App() {
+  return (
+    <SafeAreaProvider>
+      <View className="flex-1 justify-center items-center bg-white">
+        <Text className="text-5xl font-bold text-slate-600">Hello World!</Text>
+        <Text className="text-lg text-slate-500 mt-4">FSD Architecture</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}

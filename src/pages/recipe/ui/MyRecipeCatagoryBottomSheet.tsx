f297a36e109e7ffe39af18f3fa54087381d6d93e
노 기 훈 <kiki01111@naver.com>
2025-09-05 16:41:40 +0900

    feat: myrecipe catogory bottomsheet

--- src/pages/recipe/ui/MyRecipeCatagoryBottomSheet.tsx
+++ src/pages/recipe/ui/MyRecipeCatagoryBottomSheet.tsx
@@ -1,44 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BottomSheetModal from '@/shared/ui/BottomSheetModal';
-import { TextInput, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import EditIcon from '@/assets/img/catagory/edit.svg';
+import TrashIcon from '@/assets/img/catagory/trash.svg';
 
-const MyRecipeCatagoryBottomSheet = ({
-  modalVisible,
-  setModalVisible,
-}: {
+interface Props {
   modalVisible: boolean;
   setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
-  const [newCategory, setNewCategory] = React.useState('');
+}
 
-  const handleAddCategory = () => {
+const MyRecipeCatagoryBottomSheet = ({ modalVisible, setModalVisible }: Props) => {
+  // FIXME: api로 카테고리 가져오기
+  const [originalCategory] = useState(['점심', '저녁']);
+  const [newCategory, setNewCategory] = useState('');
+
+  const handleCatagoryAdd = () => {
     // 카테고리 추가 로직
+  };
+
+  const handleCatagorySave = () => {
+    // 카테고리 저장 로직
     setModalVisible(false);
   };
 
   return (
     <BottomSheetModal visible={modalVisible} onClose={() => setModalVisible(false)} height={554}>
-      <Text className="text-gray-800 mb-4 text-lg font-bold">새 카테고리 추가</Text>
+      <Text className="mt-[40px] text-center text-[20px] font-bold text-black">카테고리 관리</Text>
 
-      <TextInput
-        className="border-gray-300 mb-4 rounded-lg border px-4 py-3 text-base"
-        placeholder="카테고리 이름을 입력하세요"
-        value={newCategory}
-        onChangeText={setNewCategory}
-        autoFocus={true}
-      />
+      {/* 기존 카테고리 */}
+      <View className="mt-[32px]">
+        <Text className="font-[Pretendard] text-[16px] font-bold leading-[20px]">
+          기존 카테고리
+        </Text>
+        <View className="mt-[16px] px-11 pb-4">
+          <Text>전체</Text>
+        </View>
+        <ScrollView className="px-11">
+          {originalCategory.map((category, index) => (
+            <View
+              key={index}
+              className="flex-row items-center justify-start gap-56 self-stretch py-4"
+            >
+              <Text>{category}</Text>
+              <View className="flex-row gap-[14px]">
+                <EditIcon width={16} height={16} />
+                <TrashIcon width={16} height={16} />
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
 
-      <View className="flex-row gap-3">
-        <TouchableOpacity
-          className="bg-gray-200 flex-1 rounded-lg py-3"
-          onPress={() => setModalVisible(false)}
-        >
-          <Text className="text-gray-700 text-center font-semibold">취소</Text>
-        </TouchableOpacity>
+      {/* 카테고리 추가 */}
+      <View className="mt-[28px] items-center">
+        <Text className="self-start font-[Pretendard] text-[16px] font-bold leading-[20px]">
+          카테고리 추가
+        </Text>
 
-        <TouchableOpacity className="flex-1 rounded-lg bg-sub1 py-3" onPress={handleAddCategory}>
-          <Text className="text-center font-semibold text-white">추가</Text>
-        </TouchableOpacity>
+        <TextInput
+          className="mt-2 h-14 w-96 items-center justify-start overflow-hidden rounded-2xl bg-g4 px-5 py-4 text-[14px] font-medium text-g2"
+          placeholder="새 카테고리 이름"
+          placeholderTextColor="#847C70"
+          value={newCategory}
+          onChangeText={setNewCategory}
+        />
+
+        {/* 버튼 그룹 */}
+        <View className="mt-4 flex-col items-center gap-3">
+          <TouchableOpacity
+            onPress={handleCatagoryAdd}
+            className="h-[50px] w-96 flex-row items-center justify-center gap-2 rounded-2xl bg-g5 px-5 py-4"
+            activeOpacity={0.8}
+          >
+            <Text className="font-['Pretendard'] text-base font-bold leading-none text-white">
+              추가하기
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={handleCatagorySave}
+            className="mt-[36px] h-[50px] w-96 flex-row items-center justify-center gap-2 rounded-2xl bg-sub1 px-5 py-4"
+          >
+            <Text className="font-['Pretendard'] text-base font-bold leading-none text-white">
+              저장하기
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </BottomSheetModal>
   );

fe8e7a24a974b55b475209ff6e8321eb3e9e1d9d
kim gisu <kgs9843@naver.com>
2025-09-17 18:34:23 +0900

    fix: husky error

--- src/shared/ui/modal/fullScreen/ModalCategoriesSection.tsx
+++ src/shared/ui/modal/fullScreen/ModalCategoriesSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ScrollView, View, Pressable } from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
 
 interface Props {
   categories?: string[];

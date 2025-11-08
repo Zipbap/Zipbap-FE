44386a2885081e64aded947d80bef7d86aff54e2
노 기 훈 <kiki01111@naver.com>
2025-11-08 15:11:24 +0900

    fix: feed type, api response에 맞게 재정의

--- src/entities/feed/model/index.ts
+++ src/entities/feed/model/index.ts
@@ -1,48 +1,91 @@
-// 기본 피드 항목
-export interface Feed {
-  id: string;
-  profileImage: string; // 프로필 사진
-  nickname: string; // 닉네임
-  title: string; // 피드 제목
-  views: number; // 조회수
-  mainImage: string; // 피드 대표 사진
-  content: string; // 내용
-  cookingTime: number; // 요리 시간 (분 단위)
-  difficulty: string; // 난이도
-  ingredientsCount: number; // 재료 개수
-  likes: number; // 좋아요 수
-  bookmarks: number; // 북마크 수
-  comments: number; // 댓글 수
-  isLiked: boolean; // 좋아요 여부
-  isBookmarked: boolean; // 북마크 여부
-  isCommented: boolean; // 댓글 여부
+// import { CookingOrder } from '@entities/recipe';
+
+import { CookingOrder } from '@/src/entities/recipe';
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
 }
 
-// 레시피 단계
-export interface RecipeStep {
-  step: number; // 단계 번호
-  title: string; // 단계 제목
-  description: string; // 단계 설명
-  image: string; // 단계 이미지 URL
+export interface Pageable {
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
 }
 
-// 상세 피드
-export interface FeedDetail extends Feed {
-  subTitle: string; // 부제
-  isFollowing: boolean; // 팔로우 여부
-  followers: number; // 팔로워 수
-  introduce: string; // 자기소개
-  createdAt: string; // 작성일
-  recipeIntroduce: string; // 레시피 소개
-  categories: string[]; // 해당 레시피 카테고리
-  serving: string; // 인분
-  ingredients: string; // 재료
-  video: string; // 레시피 영상 URL
-  steps: RecipeStep[]; // 레시피 단계
-  tip: string; // 레시피 팁
+export interface FeedListParams {
+  filter?: 'ALL' | 'TODAY' | 'HOT' | 'RECOMMEND' | 'FOLLOWING';
+  page?: number;
+  size?: number;
+  condition?: string;
 }
 
-// 댓글 요소
+interface FeedBase {
+  recipeId: string;
+  title: string;
+  thumbnail: string;
+  introduction: string;
+  cookingTime: string;
+  level: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// feed views, feed isPublic 추가 예정
+export interface Feed extends FeedBase {
+  nickname: string;
+  profileImage: string;
+  userIsPrivate: boolean;
+  isLiked: boolean;
+  isBookmarked: boolean;
+  likeCount: number;
+  bookmarkCount: number;
+  commentCount: number;
+}
+
+// my recipe, cookingOrders 추가 예정
+export interface FeedDetail extends FeedBase {
+  nickname: string;
+  profileImage: string;
+  statusMessage: string;
+  isFollowing: boolean;
+  subtitle: string;
+  video: string;
+  ingredientInfo: string;
+  kick: string;
+  recipeIsPrivate: boolean;
+  recipeStatus: string;
+  cookingType: string;
+  situation: string;
+  mainIngredient: string;
+  method: string;
+  headcount: string;
+  likeCount: number;
+  isLiked: boolean;
+  bookmarkCount: number;
+  isBookmarked: boolean;
+  commentCount: number;
+  cookingOrders: CookingOrder[];
+}
+
+export interface FeedPage {
+  content: Feed[];
+  pageable: Pageable;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
 export interface Comment {
   id: string; // 댓글 고유 ID (문자열)
   nickname: string; // 작성자 이름

f74fa125a6cf6c461dff29b7b428b33124c6414c
노 기 훈 <kiki01111@naver.com>
2025-12-01 16:21:53 +0900

    feat: prefetch hook chunk 적용

--- src/shared/lib/usePrefetchImages.ts
+++ src/shared/lib/usePrefetchImages.ts
@@ -3,26 +3,32 @@ import { useEffect, useMemo } from 'react';
 
 type PrefetchableImages = string | undefined | null;
 
+const CHUNK_SIZE = 10;
+
 export const usePrefetchImages = (imageUrls: readonly PrefetchableImages[]) => {
   const stringifiedUrls = useMemo(() => JSON.stringify(imageUrls), [imageUrls]);
 
   useEffect(() => {
     const urls: PrefetchableImages[] = JSON.parse(stringifiedUrls);
-    const validUrls = urls.filter((url): url is string => !!url);
+    const prefetchUrls = urls.filter((url): url is string => !!url);
 
-    if (validUrls.length === 0) return;
+    if (prefetchUrls.length === 0) return;
 
-    const prefetchImages = async () => {
-      const prefetchPromises = validUrls.map(async url => {
-        const cachePath = await Image.getCachePathAsync(url);
-        if (!cachePath) {
-          return Image.prefetch(url);
-        }
-      });
+    const executePrefetchInChunks = async () => {
+      for (let i = 0; i < prefetchUrls.length; i += CHUNK_SIZE) {
+        const chunk = prefetchUrls.slice(i, i + CHUNK_SIZE);
 
-      await Promise.allSettled(prefetchPromises);
+        const prefetchTasks = chunk.map(async url => {
+          const cachePath = await Image.getCachePathAsync(url);
+          if (!cachePath) {
+            return Image.prefetch(url);
+          }
+        });
+
+        await Promise.allSettled(prefetchTasks);
+      }
     };
 
-    prefetchImages();
+    executePrefetchInChunks();
   }, [stringifiedUrls]);
 };
